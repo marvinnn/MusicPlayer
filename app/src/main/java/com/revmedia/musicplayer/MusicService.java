f9862229638f95a4301b77897d8a924070a6d4ba
marvinnn <marvin12@mhs.if.its.ac.id>
2016-08-04 10:00:52 +0700

    Create service

---
+++ app/src/main/java/com/revmedia/musicplayer/MusicService.java
@@ -0,0 +1,72 @@
+package com.revmedia.musicplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import java.util.ArrayList;
+import android.content.ContentUris;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.PowerManager;
+import android.util.Log;
+
+/**
+ * Created by Marvin Zeson on 8/4/2016.
+ */
+public class MusicService extends Service implements
+        MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
+        MediaPlayer.OnCompletionListener{
+    private MediaPlayer player;
+    private ArrayList<Song> songs;
+    private int songPosn;
+
+    public void onCreate(){
+        super.onCreate();
+        songPosn = 0;
+        player = new MediaPlayer();
+        initMusicPlayer();
+    }
+
+    public void initMusicPlayer(){
+        player.setWakeMode(getApplicationContext(),
+                PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+    }
+
+    public void setList(ArrayList<Song> theSongs){
+        songs=theSongs;
+    }
+
+    public class MusicBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+
+    }
+}
